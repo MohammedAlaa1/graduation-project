cf306838c6a7fd329c3af9c35b4942a5fb880829
MohammedAlaa1 <126056161+MohammedAlaa1@users.noreply.github.com>
2023-04-23 00:54:49 +0200

    ..

--- apps/patients-app/src/app/api/trpc/generated/routers/index.ts
+++ apps/patients-app/src/app/api/trpc/generated/routers/index.ts
@@ -1,29 +1,11 @@
-import { t } from "./helpers/createRouter"
-import { patientsRouter } from "./Patient.router"
-import { doctorsRouter } from "./Doctor.router"
-import { appointmentsRouter } from "./Appointment.router"
-
-//DUMMY USER DATA / FAKE DB DATA
-let users = [
-  {
-    name: "Vic",
-    age: "34",
-  },
-  {
-    name: "Mo",
-    age: "25",
-  },
-  {
-    name: "Charlie",
-    age: "99",
-  },
-]
+import { t } from "./helpers/createRouter";
+import { patientsRouter } from "./Patient.router";
+import { doctorsRouter } from "./Doctor.router";
+import { appointmentsRouter } from "./Appointment.router";
 
 export const appRouter = t.router({
   patient: patientsRouter,
   doctor: doctorsRouter,
-  appointment: appointmentsRouter,
-  getUsers: t.procedure.query(() => {
-    return users
-  }),
+  appointment: appointmentsRouter
 })
+
