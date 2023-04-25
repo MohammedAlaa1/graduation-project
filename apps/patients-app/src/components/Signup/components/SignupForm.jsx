3aaa747976d90dfcba847c59e57bd0f431f3d46e
Vic <patriciu_victor@hotmail.com>
2023-04-26 01:19:19 +0700

    fix

--- apps/patients-app/src/components/Signup/components/SignupForm.jsx
+++ apps/patients-app/src/components/Signup/components/SignupForm.jsx
@@ -14,79 +14,82 @@ import {
 } from "@mantine/core"
 import Link from "next/link"
 
-export default function SignUpForm({form ,saveFormDataToDB}) {
-    return(
-        <Container size={420} my={40}>
-        <Title
-          align="center"
-          sx={(theme) => ({
-            fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-            fontWeight: 900,
-          })}
+export default function SignUpForm({ form, saveFormDataToDB }) {
+  return (
+    <Container size={420} my={40}>
+      <Title
+        align="center"
+        sx={(theme) => ({
+          fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+          fontWeight: 900,
+        })}
+      >
+        Welcome!
+      </Title>
+      <Text color="dimmed" size="sm" align="center" mt={5}>
+        Have an account already? <Link href="/login">log in</Link>
+      </Text>
+
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <form
+          onSubmit={form.onSubmit((values) =>
+            saveFormDataToDB({ formData: values })
+          )}
         >
-          Welcome!
-        </Title>
-        <Text color="dimmed" size="sm" align="center" mt={5}>
-          Have an account already? <Link href="/login">log in</Link>
-        </Text>
-  
-        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-          <form onSubmit={form.onSubmit((values) => saveFormDataToDB({values}))}>
-            <TextInput placeholder="Your name" label="Full name" withAsterisk             {...form.getInputProps('name')}
-   />
-            <TextInput
-              label="Email"
-              placeholder="example@gmail.com"
-              required
-              mt="md"
-              {...form.getInputProps('email')}
-  
-            />
-            <PasswordInput
-              label="Password"
-              placeholder="Your password"
-              required
-              mt="md"
-              {...form.getInputProps('password')}
-  
-            />
-            <NumberInput
-              defaultValue={18}
-              placeholder="Your age"
-              label="Your age"
-              mt="md"
-              withAsterisk
-              {...form.getInputProps('age')}
-  
-            />
-  
-  <TextInput
-              label="Address"
-              placeholder="Address"
-              required
-              mt="md"
-              {...form.getInputProps('address')}
-  
-            />
-  
-  <NumberInput
-              defaultValue={18}
-              placeholder="Phone Number"
-              label="Phone Number"
-              mt="md"
-              withAsterisk
-              {...form.getInputProps('phoneNumber')}
-  
-            />
-  
-            <Group position="apart" mt="lg">
-              <Checkbox label="Agree to the policy" />
-            </Group>
-            <Button type="submit" fullWidth mt="xl">
-              Sign up
-            </Button>
-          </form>
-        </Paper>
-      </Container>
-    )
+          <TextInput
+            placeholder="Your name"
+            label="Full name"
+            withAsterisk
+            {...form.getInputProps("name")}
+          />
+          <TextInput
+            label="Email"
+            placeholder="example@gmail.com"
+            required
+            mt="md"
+            {...form.getInputProps("email")}
+          />
+          <PasswordInput
+            label="Password"
+            placeholder="Your password"
+            required
+            mt="md"
+            {...form.getInputProps("password")}
+          />
+          <NumberInput
+            defaultValue={18}
+            placeholder="Your age"
+            label="Your age"
+            mt="md"
+            withAsterisk
+            {...form.getInputProps("age")}
+          />
+
+          <TextInput
+            label="Address"
+            placeholder="Address"
+            required
+            mt="md"
+            {...form.getInputProps("address")}
+          />
+
+          <NumberInput
+            defaultValue={18}
+            placeholder="Phone Number"
+            label="Phone Number"
+            mt="md"
+            withAsterisk
+            {...form.getInputProps("phoneNumber")}
+          />
+
+          <Group position="apart" mt="lg">
+            <Checkbox label="Agree to the policy" />
+          </Group>
+          <Button type="submit" fullWidth mt="xl">
+            Sign up
+          </Button>
+        </form>
+      </Paper>
+    </Container>
+  )
 }
