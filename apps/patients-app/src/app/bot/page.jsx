f39b7031c780988d13615f071e6dafe810b61c0b
Vic <patriciu_victor@hotmail.com>
2023-05-03 06:26:03 +0700

    fixed chatGPT API ; need to add global State

--- apps/patients-app/src/app/bot/page.jsx
+++ apps/patients-app/src/app/bot/page.jsx
@@ -1,21 +1,26 @@
-"use client"
-import { Container } from "@mantine/core"
 import Bot from "../../components/Bot/Bot"
+import { openai } from "../../lib/chatGPT/chatGPT"
+import cookieCutter from "cookie-cutter"
 
-export default function Home() {
-    return (
-      <Container
-        sx={(theme) => ({
-          height: "100vh",
-          backgroundColor:
-            theme.colorScheme === "dark"
-              ? theme.colors.dark[6]
-              : theme.colors.gray[0],
-        })}
-        w={"100%"}
-        h={"100%"}
-      >
-        <Bot/>
-      </Container>
-    )
-  }
+async function getResponse() {
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: "Who won the world series in 2020?" }],
+    temperature: 0.9,
+    max_tokens: 2048,
+    frequency_penalty: 0.5,
+    presence_penalty: 0,
+  })
+  return response
+}
+
+export default async function Home() {
+  let userInput = cookieCutter.get("userInput")
+
+  let response = await getResponse({
+    input: userInput,
+  })
+
+  let output = response?.data?.choices[0]?.message.content
+  return <Bot responseData={output} />
+}
--- apps/patients-app/src/components/Bot/Bot.jsx
+++ apps/patients-app/src/components/Bot/Bot.jsx
@@ -1,145 +1,155 @@
-import { useState } from 'react';
-import { TextInput, Button, Text } from '@mantine/core';
+"use client"
+import { useState } from "react"
+import { TextInput, Button, Text } from "@mantine/core"
+import cookieCutter from "cookie-cutter"
 
-export default function Chatbot() {
-  const [message, setMessage] = useState('');
-  const [chatHistory, setChatHistory] = useState([]);
+export default function Chatbot({ responseData }) {
+  const [message, setMessage] = useState("")
+  const [response, setResponse] = useState("")
 
-  const handleSendMessage = () => {
+  const [chatHistory, setChatHistory] = useState([])
+
+  const handleSendMessage = async () => {
     // Logic to send message to chatbot
-    setChatHistory([...chatHistory, { sender: 'user', message }]);
-    setMessage('');
-  };
+    setChatHistory([...chatHistory, { sender: "user", message }])
+    setMessage("")
+    cookieCutter.set("userInput", message)
+    setResponse(responseData)
+  }
 
   const styles = {
     container: {
-        maxWidth: 600,
-        margin: 'auto',
-        padding: 16,
-        backgroundColor: '#FFFFFF',
-        minHeight: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
-        borderRadius: 8,
-      },
-      header: {
-        textAlign: 'center',
-        marginBottom: 32,
-        color: '#333333',
-        fontWeight: 700,
-        fontSize: 48,
-        textTransform: 'uppercase',
-        letterSpacing: 2,
-      },
-      chatContainer: {
-        height: 400,
-        width: '100%',
-        maxWidth: 600,
-        overflowY: 'scroll',
-        marginBottom: 32,
-        borderRadius: 8,
-        backgroundColor: '#F7F7F7',
-        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'flex-end',
-        padding: 16,
-      },
-      chatMessage: {
-        backgroundColor: '#2B8BFF',
-        color: '#FFFFFF',
-        borderRadius: 16,
-        padding: '12px 16px',
-        marginBottom: 16,
-        alignSelf: 'flex-start',
-        maxWidth: '60%',
-        wordWrap: 'break-word',
-        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
-        animation: '$fadeIn 0.3s ease-in-out',
-      },
-      chatbotMessage: {
-        backgroundColor: '#EFEFEF',
-        borderRadius: 16,
-        padding: '12px 16px',
-        marginBottom: 16,
-        alignSelf: 'flex-end',
-        maxWidth: '60%',
-        wordWrap: 'break-word',
-        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
-        animation: '$fadeIn 0.3s ease-in-out',
-      },
-      messageContainer: {
-        display: 'flex',
-        alignItems: 'center',
-        marginBottom: 32,
-        width: '100%',
-        maxWidth: 600,
-      },
-      input: {
-        flex: 1,
-        marginRight: 8,
-        borderRadius: 16,
-        borderColor: '#EFEFEF',
-        backgroundColor: '#F7F7F7',
-        padding: '12px 16px',
-        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
-        fontSize: 18,
-        ':focus': {
-          borderColor: '#2B8BFF',
-          boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
-        },
-      },
-      button: {
-        borderRadius: 16,
-        backgroundColor: '#2B8BFF',
-        color: '#FFFFFF',
-        fontWeight: 'bold',
-        padding: '12px 24px',
-        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
-        fontSize: 18,
-      ':hover': {
-        backgroundColor: '#1870',
+      maxWidth: 600,
+      margin: "auto",
+      padding: 16,
+      backgroundColor: "#FFFFFF",
+      minHeight: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+      borderRadius: 8,
+    },
+    header: {
+      textAlign: "center",
+      marginBottom: 32,
+      color: "#333333",
+      fontWeight: 700,
+      fontSize: 48,
+      textTransform: "uppercase",
+      letterSpacing: 2,
+    },
+    chatContainer: {
+      height: 400,
+      width: "100%",
+      maxWidth: 600,
+      overflowY: "scroll",
+      marginBottom: 32,
+      borderRadius: 8,
+      backgroundColor: "#F7F7F7",
+      boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "flex-end",
+      padding: 16,
+    },
+    chatMessage: {
+      backgroundColor: "#2B8BFF",
+      color: "#FFFFFF",
+      borderRadius: 16,
+      padding: "12px 16px",
+      marginBottom: 16,
+      alignSelf: "flex-start",
+      maxWidth: "60%",
+      wordWrap: "break-word",
+      boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+      animation: "$fadeIn 0.3s ease-in-out",
+    },
+    chatbotMessage: {
+      backgroundColor: "#EFEFEF",
+      borderRadius: 16,
+      padding: "12px 16px",
+      marginBottom: 16,
+      alignSelf: "flex-end",
+      maxWidth: "60%",
+      wordWrap: "break-word",
+      boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+      animation: "$fadeIn 0.3s ease-in-out",
+    },
+    messageContainer: {
+      display: "flex",
+      alignItems: "center",
+      marginBottom: 32,
+      width: "100%",
+      maxWidth: 600,
+    },
+    input: {
+      flex: 1,
+      marginRight: 8,
+      borderRadius: 16,
+      borderColor: "#EFEFEF",
+      backgroundColor: "#F7F7F7",
+      padding: "12px 16px",
+      boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+      fontSize: 18,
+      ":focus": {
+        borderColor: "#2B8BFF",
+        boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
       },
     },
-    '@keyframes fadeIn': {
+    button: {
+      borderRadius: 16,
+      backgroundColor: "#2B8BFF",
+      color: "#FFFFFF",
+      fontWeight: "bold",
+      padding: "12px 24px",
+      boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+      fontSize: 18,
+      ":hover": {
+        backgroundColor: "#1870",
+      },
+    },
+    "@keyframes fadeIn": {
       from: { opacity: 0 },
       to: { opacity: 1 },
     },
-  };
+  }
 
-  return (    <div style={styles.container}>
-    <Text style={styles.header}>
-      Chatbot
-    </Text>
-    <div style={styles.chatContainer}>
-      {chatHistory.map((chat, index) => (
-    <div
-      key={index}
-      style={
-        chat.sender === 'user'
-          ? styles.chatMessage
-          : styles.chatbotMessage
-      }
-    >
-      <Text>{chat.message}</Text>
+  return (
+    <div style={styles.container}>
+      <Text style={styles.header}>Chatbot</Text>
+      <div style={styles.chatContainer}>
+        {chatHistory.map((chat, index) => (
+          <div
+            key={index}
+            style={
+              chat.sender === "user"
+                ? styles.chatMessage
+                : styles.chatbotMessage
+            }
+          >
+            <Text>{chat.message}</Text>
+            <Text>{chat.response}</Text>
+          </div>
+        ))}
+      </div>
+      <div style={styles.messageContainer}>
+        <TextInput
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          placeholder="Type your message here..."
+          style={styles.input}
+        />
+        <Button
+          onClick={async () => {
+            handleSendMessage()
+          }}
+          style={styles.button}
+        >
+          Send
+        </Button>
+      </div>
     </div>
-  ))}
-</div>
-<div style={styles.messageContainer}>
-  <TextInput
-    value={message}
-    onChange={(event) => setMessage(event.target.value)}
-    placeholder="Type your message here..."
-    style={styles.input}
-  />
-  <Button
-    onClick={handleSendMessage}
-    style={styles.button}
-  >
-    Send
-  </Button>
-</div>
-</div>)}
+  )
+}
